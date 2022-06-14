c66eac2682ef2f4bbe6227375e711267d502f6dd
Lyssa <ebonwolf00@gmail.com>
2022-06-14 15:21:07 +0100

    react-router-dom installed and path errors resolved

--- src/Containers/Dashboard/Dashboard.jsx
+++ src/Containers/Dashboard/Dashboard.jsx
@@ -1,6 +1,12 @@
 import './Dashboard.scss';
 import NavBar from '../../Components/NavBar/NavBar';
 import { Routes, Route } from 'react-router-dom';
+import Home from '../../Pages/Home/Home';
+import About from '../../Pages/About/About';
+import Music from '../../Pages/Music/Music';
+import Contact from '../../Pages/Contact/Contact';
+import Forum from '../../Pages/Forum/Forum';
+import Merch from '../../Pages/Merch/Merch';
 
 const Dashboard = () => {
   return (
@@ -8,11 +14,11 @@ const Dashboard = () => {
       <NavBar />
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/about' />
-        <Route path='/music' />
-        <Route path='/contact' />
-        <Route path='/forum' />
-        <Route path='/merch' />
+        <Route path='/about' element={<About />} />
+        <Route path='/music' element={<Music />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/forum' element={<Forum />} />
+        <Route path='/merch' element={<Merch />} />
       </Routes>
     </div>
   );
