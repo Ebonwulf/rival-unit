6c6dc310f7c4aaea313d23c8d274013e5213c388
Lyssa <ebonwolf00@gmail.com>
2022-06-19 17:05:42 +0100

    removed extra image from contact page

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,7 +1,6 @@
 import './Contact.scss';
 import ContactForm from '../../Components/ContactForm/ContactForm';
 import GigPic from '../../Assets/Images/RU-gig-pic.jpg';
-import CassettePic from '../../Assets/Images/ru-cassette-img.png';
 
 const Contact = () => {
   return (
@@ -10,7 +9,6 @@ const Contact = () => {
       <div className='contact__container'>
         <img className='contact__gig-image' src={GigPic} alt='Gig' />
         <ContactForm />
-        <img className='contact__cassette' src={CassettePic} alt='cassette' />
       </div>
     </div>
   );
