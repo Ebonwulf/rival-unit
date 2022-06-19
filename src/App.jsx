01bc884b960f2fc271cc018bccae1895e0af6a84
Lyssa <ebonwolf00@gmail.com>
2022-06-19 09:12:26 +0100

    footer component created and jsx and scss done. Image added to the home page. Footer component called in the App to allow it to appear on all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import './App.scss';
+import Footer from './Components/Footer/Footer';
 import Dashboard from './Containers/Dashboard/Dashboard';
 
 const App = () => {
-  return <Dashboard />;
+  return (
+    <>
+      <Dashboard />
+      <Footer />
+    </>
+  );
 };
 
 export default App;
